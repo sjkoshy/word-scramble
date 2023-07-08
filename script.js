358894f7d1b89dd3d61e0c347509f789c8af2d9f
Sherilyn Koshy <sherilynkoshy@Sherilyns-MBP.lan>
2023-07-08 09:42:40 -0400

    Adding index.html, script.js, and style.css files

---
+++ script.js
@@ -0,0 +1,12 @@
+const wordList = [
+  ["syntax"],
+  ["glance"],
+  ["policy"],
+  ["height"],
+  ["middle"],
+  ["window"],
+  ["bounty"],
+  ["script"],
+  ["carpet"],
+  ["object"],
+  ["branch"]]
